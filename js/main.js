2270f73418eea2dd159cd8429a02d7e5c08908c5
sabrina hall <sabrinahall@sabrinas-MacBook-Pro.local>
2018-07-07 16:43:15 -0700

    twentith commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+new Vue({
+  el: '#app',
+  data: {
+      progress: 0
+  },
+  methods: {
+    start: function() {
+      var vm = this;
+      var width = 5;
+      var increase= setInterval(function() {
+        vm.progress += width;
+        if(vm.progress >= 100) {
+          clearInterval(increase);
+        }
+
+      }, 100);
+    }
+  }
+})
